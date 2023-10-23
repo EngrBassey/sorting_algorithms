02d92494e5b08ea4ecbeff5e7fe6cf71094d156c
EngrBassey <samuelbassey001@gmail.com>
2023-10-23 14:32:26 -0700

    Bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,6 +1,22 @@
 #include "sort.h"
 
 /**
+  * swap_func - function that swaps 2 values
+  * @a: first value
+  * @b: second value
+  */
+
+void swap_func(int *a, int *b)
+{
+	int tmp;
+
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+
+/**
   * bubble_sort - function that implement bubble sort
   * @array: the elements list
   * @size: the size
